7b63ece1f50ead507ee65ef6dcb5d81b1c0bd615
Abuu001 <abulugz@gmail.com>
2021-10-11 20:43:28 +0300

    added app routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { FourthComponent } from './components/fourth/fourth.component';
+import { ThirdComponent } from './components/third/third.component';
+import { SecondComponent } from './components/second/second.component';
+import { FirstComponent } from './components/first/first.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'first',component: FirstComponent},
+  {path: 'second',component: SecondComponent},
+  {path: 'third',component: ThirdComponent},
+  {path: 'fourth',component: FourthComponent},
+  {path: '',redirectTo: '/first' , pathMatch: 'full'}, // willl redirect to first if no url is provided
+  { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) }, 
+  {path: '**',component: FirstComponent} // wildcard  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
