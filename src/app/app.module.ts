04009f4df8d33ec770468c259a96977e5e88adea
Abuu001 <abulugz@gmail.com>
2021-10-11 20:44:57 +0300

    md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,28 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TruncatePipe } from './pipes/truncate.pipe';
+import { UserComponent } from './components/user/user.component';
+import { SecondComponent } from './components/second/second.component';
+import { FirstComponent } from './components/first/first.component';
+import { ThirdComponent } from './components/third/third.component';
+import { FourthComponent } from './components/fourth/fourth.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TruncatePipe
+    TruncatePipe,
+    UserComponent,
+    SecondComponent,
+    FirstComponent,
+    ThirdComponent,
+    FourthComponent
   ],
   imports: [ 
     BrowserModule,
     AppRoutingModule,
-    FormsModule,    //when dealing with input / output
-    HttpClientModule   //to deal with rest calls
+    FormsModule,    //when dealing with input / output  -->[(ngModule)]
+    HttpClientModule   //to deal with rest calls,
   ],
   providers: [ //where all services go
     
